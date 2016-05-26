9f4ce1481dfeaa5769acd28e93a5e8c421e7a328
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2016-05-27 00:02:39 +0600

    Improvements

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,16 @@
-import AppState from './store';
+import {observable, computed} from 'mobx';
+
+class AppState {
+    @observable todos = [];
+
+    addTodo = (title) => {
+        this.todos.push({
+            title: title,
+            completed: false
+        });
+    }
+}
+
 const Store = new AppState();
 
 export default Store;
